b5b6505694e77fadbbe305c36b1fac4e644f0269
Alex P. <abphaiboon@gmail.com>
2025-12-15 20:58:16 -0800

    Added in Swagger for API documentation

--- src/SentinelStack.Api/Program.cs
+++ src/SentinelStack.Api/Program.cs
@@ -1,14 +1,28 @@
 
+using Microsoft.OpenApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container
 builder.Services.AddControllers();
 builder.Services.AddHealthChecks();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "SentinelStack API", 
+        Version = "v1"
+    });
+});
 
-// Configure the HTTP request pipeline
 var app = builder.Build();
 
-app.MapControllers();
-app.MapHealthChecks("/health");
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "SentinelStack API V1");
+    c.RoutePrefix = "swagger";
+});
 
+app.MapControllers();
 app.Run();
